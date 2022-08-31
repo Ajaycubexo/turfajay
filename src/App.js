5148d5fd9fe8c32fa7dca14a7b15493c052d4792
Ajaycubexo <ajay.chauhan@cubexo.io>
2022-08-31 18:40:20 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Slider from './Components/Sliders';
+
+
+function App() {
+  return (
+       <>
+       <Header/>
+       <Footer/>
+       <Slider/>
+       <Banner/>
+
+       
+       </>
+    );
+}
+
+export default App;
